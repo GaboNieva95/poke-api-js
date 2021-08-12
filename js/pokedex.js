3b3a049280ce84c81864090a1eba3c46eebf77b0
gabonieva <gabrielnieva95@gmail.com>
2021-08-12 18:30:13 -0300

    Add functions to poke-api

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,10 +1,10 @@
 const pokeCard = document.querySelector('[data-poke-card]');
-const pokeName = document.querySelector('[data-poke-name');
-const pokeImg = document.querySelector('[data-poke-img');
-const pokeImgContainer = document.querySelector('[data-pole-img');
-const pokeId = document.querySelector('[data-poke-id');
-const pokeTypes = document.querySelector('[data-poke-types');
-const pokeStats = document.querySelector('[data-poke-stats');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-pole-img]');
+const pokeId = document.querySelector('[data-poke-id]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeStats = document.querySelector('[data-poke-stats]');
 
 
 
@@ -41,6 +41,48 @@ const searchPokemon = event =>{
 const renderPokemonData = data => {
   const sprite = data.sprites.front_default;
   const {stats, types} = data;
-  console.log(data);
+
+
+
+  pokeName.textContent = data.name;
+  pokeImg.setAttribute('src', sprite);
+  pokeId.textContent = `N° ${data.id}`;
+  setCardColor(types);
+  renderPokemonTypes(types);
+  renderPokemonStats(stats);
+};
+
+
+const setCardColor = types => {
+  const colorOne = typeColors[types[0].type.name];
+  const colorTwo = types[1] ? typeColors[types[1].type.name] : typeColors.default;
+  pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
+  pokeImg.style.backgroundSize = `5px 8px`;
 }
 
+
+const renderPokemonTypes = types => {
+  pokeTypes.innerHTML = '';
+  types.forEach(type => {
+    const typeTextElement = document.createElement('div');
+    typeTextElement.style.color = typeColors[type.type.name];
+    typeTextElement.textContent = type.type.name;
+    pokeTypes.appendChild(typeTextElement);
+    
+  });
+}
+
+
+const renderPokemonStats = stats => {
+  pokeStats.innerHTML = '';
+  stats.forEach(stat => {
+    const statElement = document.createElement('div');
+    const statElementName = document.createElement('div');
+    const statElementAmount = document.createElement('div');
+    statElementName.textContent = stat.stat.name;
+    statElementAmount.textContent = stat.base_stat;
+    statElement.appendChild(statElementName);
+    statElement.appendChild(statElementAmount);
+    pokeStats.appendChild(statElement);
+  });
+}
